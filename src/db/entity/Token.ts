2862a18d1e229878b171d50edc71f99cbea7ba57
Ibrahim Khawar <ibrahim@Ibrahims-MacBook-Pro.local>
2022-01-23 14:25:11 -0500

    Add user to table

--- src/db/entity/Token.ts
+++ src/db/entity/Token.ts
@@ -1,7 +1,7 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
 @Entity()
-export class Token {
+export class Token extends BaseEntity {
 	@PrimaryGeneratedColumn()
 	id: number;
 
--- src/router/user.route.ts
+++ src/router/user.route.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
+import { User } from "../db/entity/User";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/", async (req: Request, res: Response) => {
 	console.log("FJLSFJLSDJK");
 	res.send("HELLO WORLD");
+	const u1: User = await User.create({ userEmail: "HFJLSJKLF" });
 });
 
 export default router;
