bbf2a55e1810f096fc7e662471ee0968df586864
Ibrahim Khawar <ibrahim@Ibrahims-MacBook-Pro.local>
2022-01-23 14:30:06 -0500

    User added

--- src/router/user.route.ts
+++ src/router/user.route.ts
@@ -1,12 +1,17 @@
 import { Router, Request, Response } from "express";
 import { User } from "../db/entity/User";
+import connection from "../db/connection";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-	console.log("FJLSFJLSDJK");
-	res.send("HELLO WORLD");
-	const u1: User = await User.create({ userEmail: "HFJLSJKLF" });
+	const user = new User();
+	user.userEmail = "BOB IS COOL";
+	user.userPassword = "FJLSFLKSD";
+	user.userProject = "FLKDJSFLDS";
+	user.userType = "ADMIN";
+	await connection.manager.save(user);
+	res.send("User added!");
 });
 
 export default router;
