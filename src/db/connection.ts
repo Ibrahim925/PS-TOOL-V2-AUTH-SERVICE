78b61862cc3340bfeacd94d439a509ce75d186dc
Ibrahim Khawar <ibrahim@Ibrahims-MacBook-Pro.local>
2022-01-23 14:44:44 -0500

    M

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,29 +3,27 @@ import { createConnection, Connection } from "typeorm";
 import "dotenv/config";
 
 let connection: Connection;
-const connect = async () => {
-	createConnection({
-		type: "mysql",
-		host: "us-cdbr-east-05.cleardb.net",
-		port: 3306,
-		username: "bfc871697a0584",
-		password: "89b32f18",
-		database: "heroku_b510b87812e7040",
-		synchronize: true,
-		entities: ["src/db/entity/**/*.ts"],
-		cli: {
-			entitiesDir: "src/db/entity",
-			migrationsDir: "src/db/migration",
-			subscribersDir: "src/db/subscriber",
-		},
+createConnection({
+	type: "mysql",
+	host: "us-cdbr-east-05.cleardb.net",
+	port: 3306,
+	username: "bfc871697a0584",
+	password: "89b32f18",
+	database: "heroku_b510b87812e7040",
+	synchronize: true,
+	entities: ["src/db/entity/**/*.ts"],
+	cli: {
+		entitiesDir: "src/db/entity",
+		migrationsDir: "src/db/migration",
+		subscribersDir: "src/db/subscriber",
+	},
+})
+	.then((returnedConnection: Connection) => {
+		connection = returnedConnection;
 	})
-		.then((returnedConnection: Connection) => {
-			connection = returnedConnection;
-		})
-		.catch((error) => {
-			console.log("____________________________ERROR:", error);
-		});
-};
+	.catch((error) => {
+		console.log("____________________________ERROR:", error);
+	});
 
-connect();
+console.log(connection);
 export default connection;
