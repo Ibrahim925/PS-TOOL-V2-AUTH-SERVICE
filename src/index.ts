62c65112ab8dcf204f55b339e1c4c0d10ceb1499
Ibrahim Khawar <ibrahim@Ibrahims-MacBook-Pro.local>
2022-01-23 14:17:32 -0500

    Updated port to bind dynamically

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,6 @@ app.use(express.json());
 
 app.use("/user", userRouter);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
