3a1c5352d08ae9c4024e6c7a47ef304dd34ac655
Ibrahim Khawar <ikb1703@gmail.com>
2022-01-23 21:14:51 -0500

    Changed sign in route

--- src/router/user.route.ts
+++ src/router/user.route.ts
@@ -6,7 +6,7 @@ const router: Router = Router();
 // Create (register) user
 router.post("/", create_admin);
 
-// Read (sign in) user
-router.get("/", user_sign_in);
+// Create user token on sign in
+router.post("/token", user_sign_in);
 
 export default router;
