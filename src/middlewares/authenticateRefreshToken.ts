7415c60b99230b26f215809a28e4c7f02bf3b8a5
Ibrahim Khawar <ikb1703@gmail.com>
2022-03-12 12:36:37 -0500

    F

--- src/middlewares/authenticateRefreshToken.ts
+++ src/middlewares/authenticateRefreshToken.ts
@@ -31,9 +31,9 @@ const authenticateRefreshToken = (
 				.findOne({ where: { token, userId: data.id } });
 
 			// Check if user with specific refresh token exists in database
-			// const user = await connection
-			// 	.getRepository(User)
-			// 	.findOne({ where: { id: data.id } });
+			const user = await connection
+				.getRepository(User)
+				.findOne({ where: { id: data.id } });
 
 			if (!refreshToken) {
 				return res.sendStatus(401);
