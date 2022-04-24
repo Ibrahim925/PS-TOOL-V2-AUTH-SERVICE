fa98ba71c37e5cead8ab06cfc069bd691c4f907b
Ibrahim Khawar <ikb1703@gmail.com>
2022-04-24 01:47:41 -0400

    EB

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -5,8 +5,8 @@ import { Response } from "express";
 import { CustomRequest, Errors } from "../types";
 import { SuccessMessage } from "../types";
 import {
-	validateEmail,
-	validateLogiSenseEmail,
+  validateEmail,
+  validateLogiSenseEmail,
 } from "../helpers/emailValidation";
 import * as bcrypt from "bcryptjs";
 import { v4 as uuid } from "uuid";
@@ -17,239 +17,243 @@ import "dotenv/config";
 
 // Initialize SMTP transporter with nodemailer
 const transporter: nodemailer.Transporter<SentMessageInfo> =
-	nodemailer.createTransport({
-		service: "gmail",
-		auth: {
-			user: process.env.email,
-			pass: process.env.password,
-		},
-	});
+  nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.email,
+      pass: process.env.password,
+    },
+  });
 
 // Used by POST /user/
 interface CreateAdminRequestBody {
-	userEmail: string;
+  userEmail: string;
 }
 
 export const create_admin = async (
-	req: CustomRequest<{}, CreateAdminRequestBody, {}>,
-	res: Response
+  req: CustomRequest<{}, CreateAdminRequestBody, {}>,
+  res: Response
 ): Promise<any> => {
-	const { userEmail } = req.body;
+  const { userEmail } = req.body;
 
-	let errors: Errors = [];
+  let errors: Errors = [];
 
-	// Check that the user's actually inputted data
-	if (!userEmail) {
-		errors.push({
-			message: "Please enter your email",
-			location: "emailInput",
-		});
+  // Check that the user's actually inputted data
+  if (!userEmail) {
+    errors.push({
+      message: "Please enter your email",
+      location: "emailInput",
+    });
 
-		return res.json(errors);
-	}
+    return res.json(errors);
+  }
 
-	// Check email format
-	if (!validateEmail(userEmail)) {
-		errors.push({
-			message: "Please enter a valid email",
-			location: "emailInput",
-		});
+  // Check email format
+  if (!validateEmail(userEmail)) {
+    errors.push({
+      message: "Please enter a valid email",
+      location: "emailInput",
+    });
 
-		return res.json(errors);
-	}
+    return res.json(errors);
+  }
 
-	if (!validateLogiSenseEmail(userEmail)) {
-		errors.push({
-			message: "Please enter a LogiSense email!",
-			location: "emailInput",
-		});
+  if (!validateLogiSenseEmail(userEmail)) {
+    errors.push({
+      message: "Please enter a LogiSense email!",
+      location: "emailInput",
+    });
 
-		return res.json(errors);
-	}
+    return res.json(errors);
+  }
 
-	// Check if user already exists
-	const foundUser = await User.findOne({ where: { userEmail } });
+  // Check if user already exists
+  try {
+    const foundUser = await User.findOne({ where: { userEmail } });
 
-	if (foundUser) {
-		errors.push({
-			message: "An account with this email already exists",
-			location: "emailInput",
-		});
-		return res.json(errors);
-	}
+    if (foundUser) {
+      errors.push({
+        message: "An account with this email already exists",
+        location: "emailInput",
+      });
+      return res.json(errors);
+    }
 
-	// Encrypt password
-	const userPassword: string = uuid();
-	const salt = await bcrypt.genSalt(10);
-	const hashedPassword = await bcrypt.hash(userPassword, salt);
+    // Encrypt password
+    const userPassword: string = uuid();
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(userPassword, salt);
 
-	// Save user
-	const newAdmin: User = new User();
-	newAdmin.userEmail = userEmail;
-	newAdmin.userPassword = hashedPassword;
-	newAdmin.userProject = null;
-	newAdmin.userType = "ADMIN";
-	await connection.manager.save(newAdmin);
+    // Save user
+    const newAdmin: User = new User();
+    newAdmin.userEmail = userEmail;
+    newAdmin.userPassword = hashedPassword;
+    newAdmin.userProject = null;
+    newAdmin.userType = "ADMIN";
+    await connection.manager.save(newAdmin);
 
-	try {
-		await transporter.sendMail({
-			to: userEmail,
-			subject: "LogiSense Account Creation",
-			text: `An account was created with this email. Your password is ${userPassword}`,
-		});
-	} catch (err) {
-		await transporter.sendMail({
-			to: userEmail,
-			subject: "LogiSense Account Creation",
-			text: `An account was created with this email. Your password is ${userPassword}`,
-		});
-	}
+    try {
+      await transporter.sendMail({
+        to: userEmail,
+        subject: "LogiSense Account Creation",
+        text: `An account was created with this email. Your password is ${userPassword}`,
+      });
+    } catch (err) {
+      await transporter.sendMail({
+        to: userEmail,
+        subject: "LogiSense Account Creation",
+        text: `An account was created with this email. Your password is ${userPassword}`,
+      });
+    }
 
-	res.send(SuccessMessage.Success);
+    res.send(SuccessMessage.Success);
+  } catch (err) {
+    console.error("ERROR WHILE CREATING ADMIN: " + err);
+  }
 };
 
 // Used by POST /user/token
 interface SignInUserRequestBody {
-	userEmail: string;
-	userPassword: string;
+  userEmail: string;
+  userPassword: string;
 }
 
 export const user_sign_in = async (
-	req: CustomRequest<{}, SignInUserRequestBody, {}>,
-	res: Response
+  req: CustomRequest<{}, SignInUserRequestBody, {}>,
+  res: Response
 ) => {
-	const { userEmail, userPassword } = req.body;
+  const { userEmail, userPassword } = req.body;
 
-	let errors: Errors = [];
+  let errors: Errors = [];
 
-	// Check that the data has been inputted
-	if (!userEmail) {
-		errors.push({ message: "Please enter your email", location: "emailInput" });
-	}
+  // Check that the data has been inputted
+  if (!userEmail) {
+    errors.push({ message: "Please enter your email", location: "emailInput" });
+  }
 
-	if (!userPassword) {
-		errors.push({
-			message: "Please enter your password",
-			location: "passwordInput",
-		});
-	}
+  if (!userPassword) {
+    errors.push({
+      message: "Please enter your password",
+      location: "passwordInput",
+    });
+  }
 
-	if (errors.length) {
-		return res.json(errors);
-	}
+  if (errors.length) {
+    return res.json(errors);
+  }
 
-	// Check if userEmail is a valid email
-	if (!validateEmail(userEmail)) {
-		errors.push({
-			message: "Please enter a valid email",
-			location: "emailInput",
-		});
+  // Check if userEmail is a valid email
+  if (!validateEmail(userEmail)) {
+    errors.push({
+      message: "Please enter a valid email",
+      location: "emailInput",
+    });
 
-		return res.json(errors);
-	}
+    return res.json(errors);
+  }
 
-	// Look for user in database
-	const foundUser = await User.findOne({
-		where: {
-			userEmail,
-		},
-	});
+  // Look for user in database
+  const foundUser = await User.findOne({
+    where: {
+      userEmail,
+    },
+  });
 
-	if (!foundUser) {
-		errors.push({
-			message: "An account with this email does not exist",
-			location: "emailInput",
-		});
+  if (!foundUser) {
+    errors.push({
+      message: "An account with this email does not exist",
+      location: "emailInput",
+    });
 
-		return res.json(errors);
-	}
+    return res.json(errors);
+  }
 
-	// Validate user's password
-	const isPasswordValid = await bcrypt.compare(
-		userPassword,
-		foundUser.userPassword
-	);
+  // Validate user's password
+  const isPasswordValid = await bcrypt.compare(
+    userPassword,
+    foundUser.userPassword
+  );
 
-	if (!isPasswordValid) {
-		errors.push({
-			message: "Password is incorrect",
-			location: "passwordInput",
-		});
+  if (!isPasswordValid) {
+    errors.push({
+      message: "Password is incorrect",
+      location: "passwordInput",
+    });
 
-		return res.json(errors);
-	}
+    return res.json(errors);
+  }
 
-	// CREATE REFRESH TOKEN
-	const refreshToken = createRefreshToken(foundUser.id);
+  // CREATE REFRESH TOKEN
+  const refreshToken = createRefreshToken(foundUser.id);
 
-	const newToken = new Token();
-	newToken.token = refreshToken;
-	newToken.userId = foundUser.id;
-	await connection.manager.save(newToken);
+  const newToken = new Token();
+  newToken.token = refreshToken;
+  newToken.userId = foundUser.id;
+  await connection.manager.save(newToken);
 
-	res.json({ refreshToken });
+  res.json({ refreshToken });
 };
 
 // Update user password
 interface UpdateUserPasswordBody {
-	userEmail: string;
-	currentPassword: string;
-	newPassword: string;
+  userEmail: string;
+  currentPassword: string;
+  newPassword: string;
 }
 
 export const update_user_password = async (
-	req: CustomRequest<{}, UpdateUserPasswordBody, {}>,
-	res: Response
+  req: CustomRequest<{}, UpdateUserPasswordBody, {}>,
+  res: Response
 ) => {
-	const { userEmail, currentPassword, newPassword } = req.body;
+  const { userEmail, currentPassword, newPassword } = req.body;
 
-	const errors: Errors = [];
+  const errors: Errors = [];
 
-	// Make sure passwords are present
-	if (!currentPassword) {
-		errors.push({
-			message: "Please enter your current password",
-			location: "currentPasswordInput",
-		});
-	}
+  // Make sure passwords are present
+  if (!currentPassword) {
+    errors.push({
+      message: "Please enter your current password",
+      location: "currentPasswordInput",
+    });
+  }
 
-	if (!newPassword) {
-		errors.push({
-			message: "Please enter your new password",
-			location: "newPasswordInput",
-		});
-	}
+  if (!newPassword) {
+    errors.push({
+      message: "Please enter your new password",
+      location: "newPasswordInput",
+    });
+  }
 
-	if (errors.length) {
-		res.json(errors);
-		return;
-	}
+  if (errors.length) {
+    res.json(errors);
+    return;
+  }
 
-	// Make sure that the password is valid
-	const foundUser = await User.findOne({
-		where: {
-			userEmail,
-		},
-	});
+  // Make sure that the password is valid
+  const foundUser = await User.findOne({
+    where: {
+      userEmail,
+    },
+  });
 
-	const isPasswordValid = await bcrypt.compare(
-		currentPassword,
-		foundUser.userPassword
-	);
+  const isPasswordValid = await bcrypt.compare(
+    currentPassword,
+    foundUser.userPassword
+  );
 
-	if (!isPasswordValid) {
-		errors.push({
-			message: "Incorrect Password",
-			location: "currentPasswordInput",
-		});
-		return res.json(errors);
-	}
+  if (!isPasswordValid) {
+    errors.push({
+      message: "Incorrect Password",
+      location: "currentPasswordInput",
+    });
+    return res.json(errors);
+  }
 
-	// Encrypt new password
-	const salt = await bcrypt.genSalt(10);
-	const encryptedPassword = await bcrypt.hash(newPassword, salt);
+  // Encrypt new password
+  const salt = await bcrypt.genSalt(10);
+  const encryptedPassword = await bcrypt.hash(newPassword, salt);
 
-	await User.update({ userEmail }, { userPassword: encryptedPassword });
+  await User.update({ userEmail }, { userPassword: encryptedPassword });
 
-	return res.json("Success");
+  return res.json("Success");
 };
